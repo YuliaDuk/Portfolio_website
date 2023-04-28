b511d65c0aba83c45a770b43d91c48a2e22561c2
YuliaDuk <yule444ka1234@mail.ru>
2023-04-28 17:34:57 +0300

    создать сайт версия 1.0

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// const buttonForContact = document.querySelector('.button_type_contact')
+// buttonForContact.addEventListener('mouseover', ()=>{
+//     changeText(buttonForContact)
+// })
+// buttonForContact.addEventListener('mouseout', ()=>{
+//     changeInitText(buttonForContact)
+// })
+// function changeText(btn){
+//     btn.textContent = "жми"
+// }
+// function changeInitText(btn){
+//     btn.textContent = "связаться со мной"
+// }
